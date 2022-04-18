cb915083c2d4085fea4694e07373ba13aca79277
almogze <almogze7@gmail.com>
2022-04-18 11:49:36 +0300

    util

--- util.py
+++ util.py
@@ -7,8 +7,7 @@ import numpy as np
 def soft_max_regression(X: np.array, W: np.array, C: np.array, j):
     if 0 < j < len(W):
         w_j = W[j]
-        X_t = X.transpose()
-        X_tW = X_t @ W
+        X_tW = X.transpose() @ W
         divisor = np.sum(np.exp(X_tW), axis=0)
         return np.sum(C * np.log(np.exp(X_tW) / divisor), axis=0)
     else:
